4aeae65a931f4c08f189b0959669b6bac58e8224
CityGecko <44513201+CityGecko@users.noreply.github.com>
2020-02-15 18:50:09 +0100

    Namespace change
    Changed Namespace to AdvancedVehicleOptionsUID (Updated for Industries DLC)

--- AdvancedVehicleOptions/VehicleOptions.cs
+++ AdvancedVehicleOptions/VehicleOptions.cs
@@ -10,7 +10,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Reflection;
 
-namespace AdvancedVehicleOptions
+namespace AdvancedVehicleOptionsUID
 {
     public class VehicleOptions : IComparable
     {
